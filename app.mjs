25e0ba0913a4d551837e2e5adb62f7b908ffec90
huxiaohuanzhai <3577003918@qq.com>
2024-04-09 19:56:17 -0400

    finish a single form

--- app.mjs
+++ app.mjs
@@ -1,12 +1,36 @@
 import './config.mjs';
-import './db.mjs';import express from 'express'
+import './db.mjs'; import express from 'express'
 import path from 'path'
 import mongoose from 'mongoose';
 import session from 'express-session';
 import { fileURLToPath } from 'url';
 
 const app = express();
+app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: false }))
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
 
-app.listen(process.env.PORT || 3000);
+app.get('/', (req, res) => {
+    res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    res.redirect("/main");
+});
+
+app.get("/main", (req, res) => {
+    res.render("studentMain")
+});
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("The server is running");
+    console.log("Press control+c to stop");
+});
